ed594f81226f2873ecd8899463102ec3540e865c
adinokd6 <adinokd6@gmail.com>
2022-04-27 02:29:35 -0700

    Added association to subject

--- MainFiles/app/models/subject.rb
+++ MainFiles/app/models/subject.rb
@@ -1,6 +1,7 @@
 class Subject < ApplicationRecord
   has_many :grades
+  has_many :classes
+
   belongs_to :teacher, optional: true
   belongs_to :trainer, optional: true
-  swagger_controller :subjects, 'Subjects'
 end
